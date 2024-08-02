9d8ebff3250e05d6449878fdab789a9a8762cf18
SahilBoda07 <sahilboda07@gmail.com>
2024-08-02 17:34:51 +0530

    Just Rendom

---
+++ Niya Infotech Web/Niya Infotech Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Niya_Infotech_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
